607f0f63bbf90d7034bb0369b4e4ac10d4cac515
Beatrys Molina <beatrys.2512@gmail.com>
2022-12-30 20:11:28 -0300

    Atualização

---
+++ Desafio.java
@@ -0,0 +1,11 @@
+public class Desafio {
+
+    public static void main(String[] args) {
+
+        int M = 500;
+        int D = 195;
+        int porcentagem = (100 - ((D * 100)/M));
+
+        System.out.println("O desconto foi de " + porcentagem + "%");
+    }
+}
